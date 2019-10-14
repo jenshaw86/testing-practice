beb6ac827dd785bc45449e9aae936b367535d9d0
jenshaw86 <bloo.j.shaw@gmail.com>
2019-10-14 13:51:10 -0700

    Set up practice file and test

---
+++ practice.js
@@ -0,0 +1,17 @@
+// Write tests for the following functions, and then make the tests pass!
+
+// capitalize(string) takes a string and returns that string with the first character capitalized.
+
+// reverseString(string) takes a string and returns it reversed.
+
+// A calculator object that contains the basic operations: add, subtract, divide, and multiply.
+
+// Caesar Cipher. Read about it on this website
+
+// Don’t forget to test wrapping from z to a.
+// Don’t forget to test keeping the same case.
+// Don’t forget to test punctuation!
+// For this one, you may want to split the final function into a few smaller functions. One concept of Testing is that you don’t need to explicitly test every function you write… Just the public ones. So in this case you only need tests for the final caesar() function. If it works as expected you can rest assured that your smaller helper functions are doing what they’re supposed to.
+// Array Analysis. Write a function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
+
+module.export = practice;
---
+++ practice.test.js
@@ -0,0 +1,1 @@
+const practice = require('./practice')
