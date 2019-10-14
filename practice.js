694cd0be36bf784ebb6732186b59117ae7987590
jenshaw86 <bloo.j.shaw@gmail.com>
2019-10-14 15:05:38 -0700

    Add capitalize, reverseString, calculator. In progress caesarCipher

--- practice.js
+++ practice.js
@@ -1,12 +1,56 @@
-// Write tests for the following functions, and then make the tests pass!
+function capitalize(str) {
+  let newStr = str.toLowerCase();
+  newStr = newStr.charAt(0).toUpperCase().concat(newStr.slice(1))
+  return newStr;
+}
 
-// capitalize(string) takes a string and returns that string with the first character capitalized.
+function reverseString(str) {
+  newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    newStr = str[i].concat(newStr)
+  }
+  return newStr;
+}
 
-// reverseString(string) takes a string and returns it reversed.
+const calculator = {
+  add: (a,b) => {
+    return a+b;
+  },
+  subtract: (a,b) => {
+    return a-b;
+  },
+  multiply: (a,b) => {
+    return a*b;
+  },
+  divide: (a,b) => {
+    return a/b;
+  }
+}
 
-// A calculator object that contains the basic operations: add, subtract, divide, and multiply.
+function caesarCipher(num, word){
+  let encryption = "";
+  for (let i = 0; i < word.length; i++) {
+    let origCharCode = word.charCodeAt(i);
+    isUpperCase = true;
+    if (origCharCode > 97) isUpperCase = false;
+    
+    let shift = (origCharCode + num) % 26;
+    
+    if (isUpperCase) {
+      shift + 64
+    } else {
+      shift + 96
+    }
 
-// Caesar Cipher. Read about it on this website
+    // first, find the character code of each letter
+    // add num to character code to shift
+    // mod 26 to wrap
+    // if under 97, add to 64 (caps)
+    // if over, add to 96 (lower)
+  }
+
+  return encryption;
+}
 
 // Don’t forget to test wrapping from z to a.
 // Don’t forget to test keeping the same case.
@@ -14,4 +58,9 @@
 // For this one, you may want to split the final function into a few smaller functions. One concept of Testing is that you don’t need to explicitly test every function you write… Just the public ones. So in this case you only need tests for the final caesar() function. If it works as expected you can rest assured that your smaller helper functions are doing what they’re supposed to.
 // Array Analysis. Write a function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
 
-module.export = practice;
+module.exports = {
+  capitalize: capitalize,
+  reverseString: reverseString,
+  calculator: calculator,
+  caeserCipher: caesarCipher
+}
--- practice.test.js
+++ practice.test.js
@@ -1,1 +1,41 @@
 const practice = require('./practice')
+
+it('capitalizes the first letter of a string', () => {
+  expect(practice.capitalize('aliya')).toBe('Aliya');
+})
+
+it('capitalizes first letter and lowercases rest', () => {
+  expect(practice.capitalize('PARIS')).toBe('Paris');
+})
+
+// reverseString(string) takes a string and returns it reversed.
+it('reverses a string', () => {
+  expect(practice.reverseString('Peanut')).toBe('tunaeP');
+})
+
+// A calculator object that contains the basic operations: add, subtract, divide, and multiply.
+it('adds two numbers and returns a sum', () => {
+  expect(practice.calculator.add(32,45)).toEqual(77);
+})
+
+it('subtracts two numbers and returns the difference', () => {
+  expect(practice.calculator.subtract(53,34)).toEqual(19)
+})
+
+it('multiplies two numbers and returns the product', () => {
+  expect(practice.calculator.multiply(31,3)).toEqual(93)
+})
+
+it('divides two numbers and returns the quotient', () => {
+  expect(practice.calculator.divide(72, 3)).toEqual(24)
+})
+// Caesar Cipher. Read about it on this website
+
+it('shifts characters by 3 letters', () => {
+  expect(practice.caeserCipher(3, 'zebra')).toBe('bgdtc');
+})
+// Don’t forget to test wrapping from z to a.
+// Don’t forget to test keeping the same case.
+// Don’t forget to test punctuation!
+// For this one, you may want to split the final function into a few smaller functions. One concept of Testing is that you don’t need to explicitly test every function you write… Just the public ones. So in this case you only need tests for the final caesar() function. If it works as expected you can rest assured that your smaller helper functions are doing what they’re supposed to.
+// Array Analysis. Write a function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
